93cff355d3fd39142a819b9697ab84f9241a9933
Antwan Maddox <antwan.maddox@agilethought.com>
2020-01-10 15:42:28 -0500

    intial commit

---
+++ Selenium Grid/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace Selenium_Grid
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver _driver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.SetCapability(CapabilityType.BrowserName, "chrome");
+
+            _driver = new RemoteWebDriver(new Uri("http://192.168.0.5:4444/wd/hub"), caps);
+            _driver.Navigate().GoToUrl("http://www.espn.com");
+            _driver.Close();
+
+        }
+    }
+}
